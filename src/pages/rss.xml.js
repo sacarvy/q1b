391a5cbfe8ac6541526ce3824e38e442cd60981a
sacarvy <sukhpreetben10@gmail.com>
2023-03-08 19:50:08 +0530

    migration to content collection feature of Astro 2

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss'
+import { getCollection } from 'astro:content'
+
+export async function get() {
+  const posts = await getCollection('posts')
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: 'https://peadevp.com',
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  })
+}
